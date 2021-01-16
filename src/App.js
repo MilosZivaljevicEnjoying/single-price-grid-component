80206b00a33d7d9f1eb1968c2c6420f8a594cc3c
Milos Zivaljevic <milos.zivaljevic@enjoying.rs>
2021-01-16 20:51:49 +0100

    development - a lot of layout done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import './App.css';
+
 function App() {
   return (
-      <p>
-          qEdit src/App.js and save to reload.
-      </p>
+      <div className='grid'>
+          <div className='content'>
+              <div className='community'>
+                  <h1>Join our community</h1>
+                  <h2>30-day, hassle-free money back guarantee</h2>
+                  <div>Gain access to our full library of tutorials along with expert code reviews.</div>
+                  <div>Perfect for any developers who are serious about honing their skills.</div>
+              </div>
+              <div className='subscription'>
+                  <h2>Monthly Subscription</h2>
+                  <div className='priceContainer'>
+                      <span className='price'>&#36;29 <sup>per month</sup></span>
+                      <div>Full access for less than $1 a day</div>
+                  </div>
+                  <input type='button' value='Sign Up' />
+              </div>
+              <div className='whyUs'>
+                  <h2>Why Us</h2>
+                  <ul>
+                      <li>Tutorials by industry experts</li>
+                      <li>Peer & expert code review</li>
+                      <li>Coding exercises</li>
+                      <li>Access to our GitHub repos</li>
+                      <li> Community forum</li>
+                      <li>Flashcard decks</li>
+                      <li>New videos every week</li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+
   );
 }
 
